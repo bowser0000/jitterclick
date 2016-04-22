8eb71c8697c4335e0d7ed2fc1522d80d1f80686e
Loquacious <Loquacious@users.noreply.github.com>
2016-04-22 07:35:47 -0400

    Make both counters and finish

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,17 +2,34 @@
 	d.addEventListener('DOMContentLoaded', function () {
 		d.getElementsByClassName('container')[0].style['display'] = 'block';
 		var button = d.getElementsByTagName('button')[0],
-			clicker = d.getElementsByClassName('clicker')[0];
+			clicker = d.getElementsByClassName('clicker')[0],
 			time = d.getElementsByClassName('time')[0],
 			clicks = d.getElementsByClassName('clicks')[0];
+		var timer,
+			remain = 60,
+			clicked = 0;
 		button.addEventListener('click', function () {
 			clicker.style['display'] = 'block';
 			this.style['background-color'] = '#e00';
-			this.innerText = 'Click anywhere on the screen';
-			this.className = 'after';
+			this.innerText = 'Click anywhere';
+			remain = 60;
+			time.innerText = remain;
+			clicked = 0;
+			clicks.innerText = clicked;
+			timer = setInterval(function () {
+				remain--;
+				time.innerText = remain;
+				if (remain === 0) {
+					clearInterval(timer);
+					clicker.style['display'] = 'none';
+					button.style['background-color'] = '#75C54F';
+					button.innerText = 'Press to Start Again';
+				}
+			}, 1000);
 		});
 		clicker.addEventListener('click', function () {
-			alert('It works!');
+			clicked++;
+			clicks.innerText = clicked;
 		});
 	});
 })(document);
